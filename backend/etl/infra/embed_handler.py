ef7a431fc365b4122803723e79c17ae80c4d1d35
Sjors Lockhorst <sjors@lockhorst.dev>
2024-09-24 13:37:04 +0200

    Fixed handler

--- backend/etl/infra/embed_handler.py
+++ backend/etl/infra/embed_handler.py
@@ -9,7 +9,8 @@ def handler(job):
     run_embed_stage(
         image_embedder,
         image_count=int(job["input"]["count"]),
-        batch_size=int(job["input"]["batch_size"]),
+        retrieval_batch_size=int(job["input"]["retrieval_batch_size"]),
+        embedding_batch_size=int(job["input"]["embedding_batch_size"]),
     )
     return "Ran full embedding stage, now done!"
 
