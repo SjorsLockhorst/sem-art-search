df656f621e765a1b9f6aa3cea35ce1e6866eb3e6
StefanVDWeide <stefan.vander.weide@hotmail.com>
2024-10-01 12:55:48 +0200

    Removed debugging echo statements

--- backend/etl/run_etl.sh
+++ backend/etl/run_etl.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Debug: print environment variables
-echo "POSTGRES_USER is set to: ${POSTGRES_USER}"
-echo "DATABASE_URL is set to: ${DATABASE_URL}"
-echo "Rijks is set to: ${RIJKSMUSEUM_API_KEY}"
-
 # Start the database and backend services
 docker compose up -d database backend
 
@@ -17,4 +12,4 @@ done
 >&2 echo "Postgres is up - executing ETL process"
 
 # Run the ETL process
-docker compose run --rm backend sh -c "poetry run python -m etl.main"
+docker compose run --rm backend sh -c "poetry run python -m etl.main" 
