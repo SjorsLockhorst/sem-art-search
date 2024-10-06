d70ade72deea7dab16c3c4b92cb752cdff9f4e3f
StefanVDWeide <stefan.vander.weide@hotmail.com>
2024-10-06 23:07:45 +0200

    Removed commented out code

--- backend/etl/main.py
+++ backend/etl/main.py
@@ -2,7 +2,6 @@ import asyncio
 
 from loguru import logger
 
-from etl.embed.embed import run_embed_stage
 from etl.extract import run_extract_stage
 
 
@@ -10,10 +9,6 @@ async def main():
     logger.info("Extracting Art Objects from all sources")
     run_extract_stage()
     logger.info("Finished extracting ArtObjects")
-    logger.info("Starting embedding of ArtObjects")
-    # TODO: Discuss if this is the best palce to kick-off the embed process
-    # await run_embed_stage(image_count=10_000, batch_size=8)
-    logger.info("Finished embedding and saving of ArtObjects")
 
 
 if __name__ == "__main__":
