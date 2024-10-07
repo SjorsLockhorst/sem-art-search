51f085e699e11885d3af6d50ba224558bcc8dbfb
Sjors Lockhorst <sjors@lockhorst.dev>
2024-10-07 21:41:04 +0200

    Attempt at fixing entrypoint

--- backend/start.sh
+++ backend/start.sh
@@ -3,7 +3,7 @@
 # TODO: Create another script for prod
 
 # Run the command to execute the Python file
-exec poetry run python -m db.models
+poetry run python -m db.models
 
 # Run FastAPI with hot reloading
 exec poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000 
