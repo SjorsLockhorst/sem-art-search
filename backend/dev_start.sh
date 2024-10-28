765f641cfc217db2e321ef675bec67f9618d9219
Sjors Lockhorst <sjors@lockhorst.dev>
2024-10-28 15:47:20 +0100

    Added back model seed

--- backend/dev_start.sh
+++ backend/dev_start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # TODO: Create another script for prod
+poetry run python -m db.models
 
 # Run FastAPI with hot reloading
 exec poetry run fastapi dev --host 0.0.0.0 --port 8000 --reload
