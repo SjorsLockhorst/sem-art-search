9a78436127cb62d227c3ef4e968e412413325b6f
StefanVDWeide <stefan.vander.weide@hotmail.com>
2024-11-09 10:11:58 +0100

    Implemented plausible analytics

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,19 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	devtools: { enabled: true },
-	modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxt/image"],
-	runtimeConfig: {
-		public: {
-			apiBase: "http://localhost:8000"
-		}
-	},
-	vite: {
-		build: {
-			rollupOptions: {
-				external: [
-					"sharp"
-				]
-			}
-		},
-	}
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxt/image",
+    "@nuxtjs/plausible"
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase: "http://localhost:8000"
+    }
+  },
+  plausible: {
+    // Prevent tracking on localhost
+    ignoredHostnames: ['localhost'],
+    // Set our custom host
+    apiHost: "https://analytics.artexplorer.ai",
+    // Enable tracking of outbound links. In our case links to the Rijksmuseum and Github
+    autoOutboundTracking: true
+  },
 });
