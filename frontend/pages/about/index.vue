2088d40a1243b88d23de3191bdca97cd0ce33e43
StefanVDWeide <stefan.vander.weide@hotmail.com>
2024-12-13 18:13:53 +0100

    Updated links and mobile styling

--- frontend/pages/about/index.vue
+++ frontend/pages/about/index.vue
@@ -1,34 +1,37 @@
 <template>
   <div class="p-4 min-h-screen">
-    <h1 class="text-6xl mb-8 font-semibold">About this project</h1>
+    <div class="mb-8">
+      <h1 class="text-4xl md:text-6xl font-semibold">About this project</h1>
+      <NuxtLink to="/" class="text-xl text-blue-800 mt-4">Back Home</NuxtLink>
+    </div>
     <div class="grid grid-cols-1 md:grid-cols-2 gap-x-4">
       <div>
         <div class="mb-8">
-          <h2 class="text-4xl font-medium">What is this project?</h2>
-          <p class="text-2xl">
+          <h2 class="text-3xl md:text-4xl font-medium">What is this project?</h2>
+          <p class="text-xl md:text-2xl">
             A search engine that lets you search through artworks based on a 'vibe', aka the meaning or semantics of a
             text. You can try it out on the main page <NuxtLink to="/" class="underline text-blue-800">here.</NuxtLink>
           </p>
         </div>
         <div class="mb-8">
-          <h2 class="text-4xl font-medium">How does this work?</h2>
-          <p class="text-2xl">
+          <h2 class="text-3xl md:text-4xl font-medium">How does this work?</h2>
+          <p class="text-xl md:text-2xl">
             We actully wrote a blogpost detailing this! You can find it here: <a class="underline text-blue-800"
               href="https://blog.lockhorst.dev/projects/art-search" target="_blank">Blogpost</a>
           </p>
         </div>
         <div class="mb-8">
-          <h2 class="text-4xl font-medium">Can I see the code?</h2>
-          <p class="text-2xl">
+          <h2 class="text-3xl md:text-4xl font-medium">Can I see the code?</h2>
+          <p class="text-xl md:text-2xl">
             Yes, the whole project is open source! here you go:
-            <a href="https://github.com/StefanVDWeide/modern-orange" target="_blank"
+            <a href="https://github.com/SjorsLockhorst/sem-art-search" target="_blank"
               class="underline text-blue-800">Github</a>
           </p>
         </div>
       </div>
       <div>
         <a href="https://www.rijksmuseum.nl/nl/collectie/object/Wiskundige--50173ce0e7a914f7a4a928c47cd46b42">
-          <span class="italic text-2xl ">Wiskundige
+          <span class="italic text-xl md:text-2xl">Wiskundige
             Jan Gillisz. van Vliet (vermeld op object), 1628 - 1637</span>
         </a>
         <div>
